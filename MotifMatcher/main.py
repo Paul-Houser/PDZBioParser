48e5e3e4c002860d19366c54bca164c019081ea9
JordanValgardson <45526541+JordanValgardson@users.noreply.github.com>
2018-12-15 14:31:42 -0800

    cleaned up the motif matcher code

---
+++ MotifMatcher/main.py
@@ -0,0 +1,34 @@
+import argparse
+import sys
+import motifFileMaker
+import motifCounterXMLParse
+import os
+import time
+def parseArgs():
+    parser = argparse.ArgumentParser(
+        description='TODO')
+    parser.add_argument('-refOrganism', required=True, type=str,
+                         help = "Provide the file name of the organism that the other organisms will \
+                            be compared with. Usage: /path/folder/organism.tsv")
+    parser.add_argument('-sequenceFolder',required=True, type=str,
+                        help="The path of the folder that contains tsv files that \
+                        contain the last N many residues of all the proteins in the proteomes in question.\
+                        Usage: /path/folder")
+    parser.add_argument('-numResidues', required=True, type=int, 
+                        help='The number of residues to provide statistics on. Usage: 6')
+    parser.add_argument('-inputValues', required=True,nargs='+',
+                        help='The matching positions with desired amino acids. Usage: P0:ILVF P2:ST ... PX:X')
+    return parser.parse_args()
+if __name__ == "__main__":
+    args = parseArgs()
+    print(args)
+    start = time.clock()
+    folderName = os.path.basename(os.path.normpath(args.sequenceFolder))
+    print(args.inputValues)
+    motifName ='-'.join(args.inputValues).replace(':','_')
+    motif = {(5-int(k[0])):set(list(k[1])) for k in[i.replace('P','').split(':') for i in args.inputValues]}
+    xmlFileName = folderName + "_"+motifName+".xml"
+    tsvFileName = folderName + "_"+motifName+".tsv"
+    motifFileMaker.motif_Finder(folderName+'/',motif,args.numResidues,xmlFileName,args.refOrganism)
+    motifCounterXMLParse.generateOutput(xmlFileName,tsvFileName)
+    
---
+++ MotifMatcher/motifFileMaker.py
@@ -0,0 +1,62 @@
+import os
+import xml.etree.cElementTree as ET
+import lxml.etree as etree
+#written by Jordan Valgardson
+
+def motif_Finder(inTSVFolder,motif_type,motifLen,outFile,refOrganism):
+    # takes an input folder and a reference organism computes all of the matches between
+    # the reference organism motifs and the motifs of the reference organisms
+    allEndings = {}
+    motifEndings = {}
+    writeFile = open(outFile,"wb")
+    refEndings = set()
+    # gets all of the unique protein endings for each organism in the tsv folder saves them to a dictionary
+    for file in os.listdir(inTSVFolder):
+        readFile = open(inTSVFolder + file, 'r')
+        allEndings[file] = set()
+        for line in readFile:
+            tempSequence = line.split('\t')[1].replace('\n','')
+            if len(tempSequence) == motifLen:
+                allEndings[file].add(tempSequence)
+    # checks if the protein endings fufill the motif criteria
+    for organism in allEndings:  
+        motifEndings[organism] = set()
+        for motif in allEndings[organism]:
+           
+            if all(motif[key] in motif_type[key] for key in motif_type):
+                 motifEndings[organism].add(motif)
+    # removes the reference organism from the rest of the organisms
+    refEndings = refEndings | motifEndings[refOrganism]
+    del motifEndings[refOrganism]
+    #initializes the xml tree output
+    root = etree.Element("root")
+    for Ref_motif in refEndings:
+        refEnding  = etree.SubElement(root, "RefSequence",name = Ref_motif)
+        
+        # calculates the score for every unique motif in the organism list against the reference organism
+        for organism in motifEndings:
+            tempDict = {6:set(),5:set(),4:set(),3:set(),2:set(),1:set()}
+            for motif in motifEndings[organism]:
+                score = sum([1 if motif[i] == Ref_motif[i] else 0 for i in [5,4,3,2,1,0]])
+                if score != 0:
+                    tempDict[score].add(motif)
+
+
+
+
+            NonRefOrganism = etree.SubElement(refEnding, "NonRefOrganism",name = organism)
+            # add to tree
+            for score in range(6,0,-1):
+                matchScore = etree.SubElement(NonRefOrganism, "match"+str(score)).text = ",".join(tempDict[score])
+    tree = etree.ElementTree(root)
+    #print tree to file
+    writeFile.write(etree.tostring(tree, pretty_print=True))
+    writeFile.close()
+    
+
+
+
+        
+        
+    
+    
